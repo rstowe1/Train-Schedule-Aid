3f9bd5748d8e74ffa4fcb896738c338f950829b7
Ryan Stowe <ryanwstowe@gmail.com>
2018-03-30 13:25:11 -0400

    duplicating table entries, but switching to the project

--- assets/JavaScript/logic.js
+++ assets/JavaScript/logic.js
@@ -13,28 +13,28 @@ var database = firebase.database();
 
 $('#add-train-btn').on('click', function (event) {
   event.preventDefault();
-  
+
   var tName = $('#name-input').val().trim();
   var tDest = $('#dest-input').val().trim();
   var tStart = moment($('#start-input').val().trim(), 'HH:mm').format('X');
   var tFreq = $('#freq-input').val().trim();
-  
+
   var newTrain = {
     name: tName,
     dest: tDest,
     start: tStart,
     freq: tFreq
   };
-  
+
   database.ref().push(newTrain);
-  
+
   console.log(newTrain.name);
   console.log(newTrain.dest);
   console.log(newTrain.start);
   console.log(newTrain.freq);
-  
+
   alert('You have added a new train, \n look at the table to see the schedule');
-  
+
   $('#name-input').val('');
   $('#dest-input').val('');
   $('#start-input').val('');
@@ -43,23 +43,23 @@ $('#add-train-btn').on('click', function (event) {
 
 database.ref().on('child_added', function (childSnapShot) {
   console.log(childSnapShot.val());
-  
+
   var Name = childSnapShot.val().name;
   var Dest = childSnapShot.val().dest;
   var Start = childSnapShot.val().start;
   var Freq = childSnapShot.val().freq;
-  
+
   console.log(Name);
   console.log(Dest);
   console.log(Start);
   console.log(Freq);
-  
+
   var makeItSnazzy = moment.unix(Start).format('HH:mm');
-  
-  var calculateThatTime = moment().diff(moment(makeItSnazzy, 'X'), 'HH:mm');
-  
-  var countItDown = moment().starOf(calculateThatTime).fromNow();
-  
-  $('#trainSchedule > tbody').append('<tr><td>' + Name + '</td><td>' + Dest + '</td><td>' + Freq + '</td><td>' + calculateThatTime + '</td><td>' + countItDown + '</td></tr>')
+
+  // var calculateThatTime = moment().diff(moment(makeItSnazzy, 'X'), 'HH:mm');
+
+  // var countItDown = moment().starOf(calculateThatTime).fromNow();
+
+  $('#trainSchedule > tbody').append('<tr><td>' + Name + '</td><td>' + Dest + '</td><td>' + Freq + '</td><td>' + 'fml' + '</td><td>' + 'fml' + '</td></tr>');
 });
 
